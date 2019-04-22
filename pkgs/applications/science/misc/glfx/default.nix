4b38bf83ff3940eb0bdb1d0ffd1ebd8a5e49d41c
Daisuke Sakurai <d.sakurai0@gmail.com>
2019-04-22 09:55:40 +0200

    Add GLFX

---
+++ pkgs/applications/science/misc/glfx/default.nix
@@ -0,0 +1,28 @@
+with import <nixpkgs> {};
+
+# This avoids typing `pkgs.` before each package name.
+
+# Defines a shell.
+stdenv.mkDerivation {
+    name = "glfx-0.0.20150113";
+    nativeBuildInputs = [ cmake ];
+    src = builtins.fetchGit {
+        url = "https://github.com/maizensh/glfx.git";
+        ref = "master";
+        rev = "bf1cf05101298c44a78116391b78f58d8bbac914";
+    };
+    # Sets the build inputs, i.e. what will be available in our
+    # local environment.
+    buildInputs = [
+#        qt48Full
+#        log4cplus
+#        gdal
+#        hdf5
+#        geos
+        glew
+#        freetype
+#        grib-api
+#        libGLU
+#        gsl
+    ];
+}
